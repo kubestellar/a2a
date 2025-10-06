2e855d6f247e6a1f8d9d4f958e3e81b3441ccb3d
Per0x1de-1337 <baviskarhd99@gmail.com>
2025-10-06 20:07:19 +0530

    fix issort

--- src/shared/functions/multicluster_create.py
+++ src/shared/functions/multicluster_create.py
@@ -7,8 +7,6 @@ from typing import Any, Dict, List, Optional
 from ..base_functions import BaseFunction
 
 
-
-
 @dataclass
 class MultiClusterCreateInput:
     """All parameters accepted by multicluster_create.execute."""
