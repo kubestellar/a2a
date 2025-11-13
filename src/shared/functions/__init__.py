db3d2ed7b19ad69b0a4ead35e26c1de1e1e1a8e3
Hemanshu <162025236+Per0x1de-1337@users.noreply.github.com>
2025-11-13 08:58:37 +0530

    add helm repo and list tools (#114)
    Co-authored-by: Rishi Mondal <mavrickrishi@gmail.com>

--- src/shared/functions/__init__.py
+++ src/shared/functions/__init__.py
@@ -1,13 +1,13 @@
 """Function implementations."""
 
 from src.shared.base_functions import function_registry
-from src.shared.functions.binding_policy_management import BindingPolicyManagement
 from src.shared.functions.deploy_to import DeployToFunction
 from src.shared.functions.describe_resource import DescribeResourceFunction
 from src.shared.functions.edit_resource import EditResourceFunction
 from src.shared.functions.get_cluster_labels import GetClusterLabelsFunction
 from src.shared.functions.gvrc_discovery import GVRCDiscoveryFunction
-from src.shared.functions.helm_deploy import HelmDeployFunction
+from src.shared.functions.helm.list import HelmListFunction
+from src.shared.functions.helm.repo import HelmRepoFunction
 from src.shared.functions.kubeconfig import KubeconfigFunction
 from src.shared.functions.kubestellar_management import KubeStellarManagementFunction
 from src.shared.functions.multicluster_create import MultiClusterCreateFunction
@@ -27,6 +27,10 @@ def initialize_functions():
     function_registry.register(MultiClusterCreateFunction())
     function_registry.register(MultiClusterLogsFunction())
     function_registry.register(DeployToFunction())
+
+    # Register Helm functions
+    function_registry.register(HelmRepoFunction())
+    function_registry.register(HelmListFunction())
 
     function_registry.register(EditResourceFunction())
     function_registry.register(DescribeResourceFunction())
